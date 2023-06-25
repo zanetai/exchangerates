476724275d83b32e5d5fe65c09397b0af1079532
Zaneta <zaneta.ignatz@gmail.com>
2023-06-25 10:50:15 +0200

    first commit

---
+++ exchangerates.py
@@ -0,0 +1,61 @@
+import requests
+
+def GetAction():
+    while True:
+        action = input("Do you want to buy or sell currency? For buy press 'b', for sell press 's': ").strip().lower()
+        if action == 'b':
+            return action
+        if action == 's':
+            return action
+        else:
+            print("You have to press 'b' or 's'!")
+            continue
+
+def GetCurrency():
+    while True:
+        currency = input("What currency do you want to buy/sell (ISO STANDARD?): ")
+        response = requests.get(f'http://api.nbp.pl/api/exchangerates/rates/C/{currency}/')
+        if (response.status_code != requests.codes.ok):
+            print('Something went wrong! Have you entered the currency in ISO standard?')
+            continue
+        else:
+            return response.json()
+
+
+def GetAmount():
+    while True:
+        try:
+            amount = float(input("How much of this currency do you want to buy/sell?: "))
+            if amount <= 0:
+                print("You can't buy 0 or less!")
+                continue
+        except ValueError:
+            print("You have to type a number!")
+            continue
+        else:
+            return round(amount, 2)
+
+
+def ChangeJson():
+    action = GetAction()
+    if action == "s":
+        myjson = GetCurrency()
+        rate = myjson["rates"][0]["bid"]
+        return rate, action
+    if action == "b":
+        myjson = GetCurrency()
+        rate = myjson["rates"][0]["ask"]
+        return rate, action
+
+def CountRate():
+    results = ChangeJson()
+    amount = GetAmount()
+    money = round(results[0] * amount, 2)
+    if results[1] == 'b':
+        print(f'You will pay {money} PLN')
+    if results[1] == 's':
+        print(f'You will receive {money} PLN')
+
+CountRate()
+
+
