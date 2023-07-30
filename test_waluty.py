526d75eca0fc6002962ac8ccea7d475e21483959
Zaneta <zaneta.ignatz@gmail.com>
2023-07-30 13:23:43 +0200

    second and third test

--- test_waluty.py
+++ test_waluty.py
@@ -1,4 +1,12 @@
-from exchangerates import CountRate
+# https://nbp.pl/statystyka-i-sprawozdawczosc/kursy/tabela-c/
+# can use this page to change the expected results from the second test
+
+
+import unittest
+from unittest.mock import patch
+from exchangerates import CountRate, GetCurrency
+
+
 
 def test_CountRate():
     results = (4.5014, "s")
@@ -7,5 +15,21 @@ def test_CountRate():
     actual_result = CountRate(results, amount)
 
     assert expected_result == actual_result
+
+
+
+class TestGetCurrency(unittest.TestCase):
+
+    @patch('builtins.input', return_value="EUR")
+    def test_once_GetCurrency(self, mock_input):
+        calling = GetCurrency()
+        self.assertEqual(calling, (4.3782, 4.4666))
+
+    @patch('builtins.input', side_effect=["EUR", "USD", "GBP"])
+    def test_multiple_GetCurrency(self, mock_input):
+        calling1 = GetCurrency()
+        calling2 = GetCurrency()
+        calling3 = GetCurrency()
+        self.assertTrue(calling1 == (4.3782, 4.4666) and calling2 == (3.9772, 4.0576) and calling3 == (5.1129, 5.2161))
 
 
